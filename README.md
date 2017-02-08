81ccccd30f3d7305e311889734a4d24bbc393f8f
Jayaram K <kurapatijayaram@gmail.com>
2017-02-08 23:34:38 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# angular2 quick start.
---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+              <h1>Angular2 App</h1>{{status}}
+            
+              `
+})
+export class AppComponent{
+}
